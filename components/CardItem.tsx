dce186f833f33891f6e1667956b9d77679b1330d
Tin Maung Zin <tinmaungzin.tmz@gmail.com>
2024-02-21 16:57:39 +0700

    fix: change ribbon style

--- components/CardItem.tsx
+++ components/CardItem.tsx
@@ -20,17 +20,67 @@ const CardWrapper = styled.div`
     opacity: 1;
   }
 `;
-
-const Ribbon = styled.div`
+const BaseRibbon = styled.div`
   position: absolute;
-  right: 0;
-  top: 0;
-  padding: 5px 10px;
-  background-color: #8dc63f;
-  color: white;
-  font-size: 12px;
-  font-weight: bold;
-  text-transform: uppercase;
+  top: -15px;
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+
+  &::before,
+  &::after {
+    position: absolute;
+    z-index: -1;
+    content: "";
+    display: block;
+    border: 5px solid #8dc63f;
+    border-top-color: transparent;
+    border-right-color: transparent;
+  }
+
+  &::before {
+    top: 0;
+    left: 0;
+  }
+
+  &::after {
+    bottom: 0;
+    right: 0;
+  }
+
+  & span {
+    position: absolute;
+    display: block;
+    width: 225px;
+    padding: 8px 0;
+
+    color: #fff;
+    font: 700 12px/1 "Lato", sans-serif;
+    text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+    text-transform: uppercase;
+    text-align: center;
+    top: 30px;
+  }
+`;
+
+const Ribbon = styled(BaseRibbon)`
+  right: -15px;
+
+  & span {
+    left: -10px;
+    background-color: #8dc63f;
+    transform: rotate(45deg);
+  }
+`;
+
+const LeftRibbon = styled(BaseRibbon)`
+  left: -15px;
+
+  & span {
+    right: -10px;
+    background-color: #993fc6;
+    transform: rotate(320deg);
+  }
 `;
 const GameInfo = styled.div`
   position: absolute;
@@ -95,20 +145,21 @@ type CardItemProps = {
   jackpots: Jackpot[];
 };
 
-const CardItem: React.FC<CardItemProps> = ({ game, activeCategory, jackpots }) => {
+const CardItem: React.FC<CardItemProps> = ({
+  game,
+  activeCategory,
+  jackpots,
+}) => {
   const imageUrl = game.image.startsWith("//")
     ? `https:${game.image}`
     : game.image;
 
   const hasRibbon =
-    activeCategory !== "top" &&
-    activeCategory !== "new" &&
-    (game.categories.includes("top") || game.categories.includes("new"));
+    game.categories.includes("top") || game.categories.includes("new");
 
   const ribbonText = game.categories.includes("top") ? "top" : "new";
 
   const [jackpot, setJackpot] = useState<Jackpot | null>(null);
-
 
   useEffect(() => {
     const jackpot = jackpots.find((jackpot) => jackpot.game === game.id);
@@ -119,7 +170,18 @@ const CardItem: React.FC<CardItemProps> = ({ game, activeCategory, jackpots }) =
     <CardWrapper>
       {jackpot && <Banner>£ {jackpot.amount}</Banner>}
 
-      {hasRibbon && <Ribbon>{ribbonText}</Ribbon>}
+      {hasRibbon &&
+        (ribbonText === "top" &&
+        (activeCategory === "new" || activeCategory !== "top") ? (
+          <LeftRibbon>
+            <span>{ribbonText}</span>
+          </LeftRibbon>
+        ) : (ribbonText === "new" && activeCategory === "top") ||
+          (activeCategory !== "new" && activeCategory !== "top") ? (
+          <Ribbon>
+            <span>{ribbonText}</span>
+          </Ribbon>
+        ) : null)}
       <GameImage
         src={imageUrl}
         alt={game.name}
